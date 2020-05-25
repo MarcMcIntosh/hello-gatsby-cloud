c05b750b84c7dcb7c846620a2d7087fb86354f4d
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-05-25 17:39:22 +0200

    task: add links to dynamicacll generated pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,69 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+export const query = graphql`{
+  prismicHomepage {
+      id
+      data {
+        featured_post {
+          document {
+            ... on PrismicBlogPost {
+              id
+              url
+              data {
+                ritchtext {
+                  raw
+                }
+              }
+            }
+          }
+        }
+        title {
+          html
+          text
+        }
+      }
+  }
+  allPrismicBlogPost {
+    edges {
+      node {
+        id
+        url
+      }
+    }
+  }
+}`;
+
+
+const IndexPage = ({ data }) => {
+  const homePageData = data.prismicHomepage.data
+
+  return (<Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>{homePageData.title.text}</h1>
+
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+
+
+    <div>
+      <h2>Blog</h2>
+        <ul>
+          
+          { data.allPrismicBlogPost.edges.map(e => (
+            <li key={e.node.id}><Link to={e.node.url}>{e.node.url}</Link></li>
+          ))}
+        </ul>
+      </div>
+  </Layout>)
+}
 
 export default IndexPage
